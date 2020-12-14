76929888d414d2080da48b93c20324eda6420476
Igor_Win10 <reid09@mail.ru>
2020-12-14 10:19:21 +0300

    try

--- api/app/jobs/send_players_pos_job.rb
+++ api/app/jobs/send_players_pos_job.rb
@@ -1,19 +1,20 @@
 class SendPlayersPosJob < ApplicationJob
   queue_as :high_priority
   # @@performing = false
-  @@thread
+  # @@thread
   def perform(*args)
     # @@performing = true
     redis = Redis.new(host: 'redis', port: 6379, db: 15)
-    @@thread = Thread.new do
+    # @@thread = Thread.new do
     #   while @@performing
         # mes = redis.mget(redis.keys('pl[0-9]*')).map { |pos| {id=>} }
         p keys = redis.keys('pl[0-9]*')
         ActionCable.server.broadcast 'some', keys.empty? ? [] : redis.mget(keys)
+        sleep 0.1
         SendPlayersPosJob.perform_later
         # sleep 0.1
     #   end
-    end
+    # end
   end
 
   # def self.perform?
